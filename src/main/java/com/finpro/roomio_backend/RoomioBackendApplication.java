a1f8a72c415b10e18ab6a8bbdcfbefab99fbec62
tizetasmnd <tizetasmnd@gmail.com>
2024-08-18 18:05:00 +0700

    init project

---
+++ src/main/java/com/finpro/roomio_backend/RoomioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finpro.roomio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finpro/roomio_backend/RoomioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finpro.roomio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
